e7076b674fa17fd844762af75377748055e15542
Artemii <artemiithefrog95@icloud.com>
2024-03-31 11:54:22 +0200

    Shop page

---
+++ lib/pages/shop_page.dart
@@ -0,0 +1,59 @@
+import 'package:empat_project_5/colors.dart';
+import 'package:empat_project_5/components/my_drawer.dart';
+import 'package:empat_project_5/components/my_product_tile.dart';
+import 'package:empat_project_5/models/shop.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class ShopPage extends StatelessWidget {
+  const ShopPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final products = context.watch<Shop>().shop;
+
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: backgroundColor,
+          elevation: 0,
+          foregroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text('Shop Page'),
+          actions: [
+            IconButton(
+              onPressed: () => Navigator.pushNamed(context, '/cart_page'),
+              icon: const Icon(Icons.shopping_cart_outlined),
+            )
+          ],
+        ),
+        drawer: const MyDrawer(),
+        backgroundColor: Theme.of(context).colorScheme.background,
+        body: ListView(
+          children: [
+            const SizedBox(
+              height: 25,
+            ),
+            Center(
+              child: Text(
+                'Pick from a selected list of premium products',
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 550,
+              child: ListView.builder(
+                itemCount: products.length,
+                scrollDirection: Axis.horizontal,
+                padding: const EdgeInsets.all(15),
+                itemBuilder: (context, index) {
+                  final product = products[index];
+
+                  return MyProductTile(product: product);
+                },
+              ),
+            ),
+          ],
+        ));
+  }
+}
