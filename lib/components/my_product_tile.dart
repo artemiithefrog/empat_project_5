1da43199120a43a909fbd6b7dcb6df2cc4b9c485
Artemii <artemiithefrog95@icloud.com>
2024-03-31 11:55:09 +0200

    Product tile

---
+++ lib/components/my_product_tile.dart
@@ -0,0 +1,104 @@
+import 'package:empat_project_5/models/product.dart';
+import 'package:empat_project_5/models/shop.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
+
+class MyProductTile extends StatelessWidget {
+  final Product product;
+
+  const MyProductTile({super.key, required this.product});
+
+  void addToCart(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: Text('Add this item to your cart?'),
+        actions: [
+          MaterialButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text('Cancel'),
+          ),
+          MaterialButton(
+            onPressed: () {
+              Navigator.pop(context);
+              context.read<Shop>().addToCart(product);
+            },
+            child: Text('Yes'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.primary,
+          borderRadius: BorderRadius.circular(12)),
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(25),
+      width: 300,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            children: [
+              AspectRatio(
+                aspectRatio: 1,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.secondary,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  width: double.infinity,
+                  padding: const EdgeInsets.all(25),
+                  child: const Icon(Icons.favorite),
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              Text(
+                product.name,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                product.description,
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('\$${product.price.toStringAsFixed(2)}'),
+              Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.secondary,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: IconButton(
+                    onPressed: () => addToCart(context),
+                    icon: const Icon(Icons.add),
+                  ))
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
