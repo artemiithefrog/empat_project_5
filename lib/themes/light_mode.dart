ea9c638c23c1b911cc8ce55ec49b3292a49447be
Artemii <artemiithefrog95@icloud.com>
2024-03-31 11:54:10 +0200

    Light Mode

---
+++ lib/themes/light_mode.dart
@@ -0,0 +1,11 @@
+import 'package:empat_project_5/colors.dart';
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+  colorScheme: const ColorScheme.light(
+    background: lightGrey,
+    primary: lightestGrey,
+    secondary: white,
+    inversePrimary: darkGrey,
+  ),
+);
