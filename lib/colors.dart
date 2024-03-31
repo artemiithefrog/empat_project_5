6b6a6bb51b4a3da7fc17ca5e89a7a6026959490a
Artemii <artemiithefrog95@icloud.com>
2024-03-31 11:55:49 +0200

    Colors

---
+++ lib/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color lightestGrey = Color(0xFFE0E0E0);
+const Color lightGrey = Color(0xFFBDBDBD);
+const Color darkGrey = Color(0xFF424242);
+const Color white = Colors.white;
+const Color grey = Colors.grey;
+
+const Color backgroundColor = Colors.transparent;
